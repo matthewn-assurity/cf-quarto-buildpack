52c27620cb5451fb4a9b9a7b28f77e6550be17d4
Matthew Nocete <matthew.nocete@assurity.co.nz>
2024-07-08 11:56:08 +1200

    update to correct download link

--- parameters.sh
+++ parameters.sh
@@ -1,10 +1,10 @@
 # This file is for config variables, and is designed to be sourced by the buildpack scripts.
 
 # supercronic setup
-# Latest releases available at https://github.com/aptible/supercronic/releases
+
 QUARTO_VERSION="v1.5.53"
 QUARTO_FILE=quarto-1.5.53-linux-amd64.tar.gz
-QUARTO_URL=https://github.com/aptible/supercronic/releases/download/${QUARTO_VERSION}/quarto-1.5.53-linux-amd64.tar.gz
+QUARTO_URL=https://github.com/quarto-dev/quarto-cli/releases/download/${QUARTO_VERSION}/quarto-1.5.53-linux-amd64.tar.gz
 
 
 # Dependencies path
